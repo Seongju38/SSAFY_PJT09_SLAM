2e2ef0398d0b43be3c2ed1f5a55734317f1c1794
Seongju38 <ljg013@naver.com>
2025-11-21 16:09:29 +0900

    create a launch file to open the created gazebo world

---
+++ README.md
@@ -0,0 +1,6 @@
+# How to run
+
+### gazebo world 실행
+```bash
+ros2 launch ./workshop.launch.py
+```
---
+++ workshop.launch.py
@@ -0,0 +1,71 @@
+## 아래 코드를 환경에 맞춰 고친 후 터미널 켜기
+## 터미널 1
+## cd (작업 중 폴더)/
+## source ~/.bashrc
+## ros2 launch ./workshop.launch.py
+
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
+def generate_launch_description():
+    launch_file_dir = os.path.join(get_package_share_directory('turtlebot3_gazebo'), 'launch')
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+
+    # 로봇 스폰 위치 수정
+    x_pose = LaunchConfiguration('x_pose', default='0') # DEFAULT 값을 수정하세요
+    y_pose = LaunchConfiguration('y_pose', default='0') # DEFAULT 값을 수정하세요
+
+    # .world 파일을 같은 해당 workshop.launch.py 파일과 같은 위치에 두세요
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+
+    world = os.path.join(
+        current_dir,
+        'workshop.world' # 사용하는 .world 파일 이름으로 교체
+    )
+
+    gzserver_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')
+        ),
+        launch_arguments={'world': world}.items()
+    )
+
+    gzclient_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')
+        )
+    )
+
+    robot_state_publisher_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(launch_file_dir, 'robot_state_publisher.launch.py')
+        ),
+        launch_arguments={'use_sim_time': use_sim_time}.items()
+    )
+
+    spawn_turtlebot_cmd = IncludeLaunchDescription(
+
+        PythonLaunchDescriptionSource(
+            os.path.join(launch_file_dir, 'spawn_turtlebot3.launch.py')
+        ),
+        launch_arguments={
+            'x_pose': x_pose,
+            'y_pose': y_pose
+        }.items()
+    )
+
+    ld = LaunchDescription()
+
+    # Add the commands to the launch description
+    ld.add_action(gzserver_cmd)
+    ld.add_action(gzclient_cmd)
+    ld.add_action(robot_state_publisher_cmd)
+    ld.add_action(spawn_turtlebot_cmd)
+
+    return ld 
